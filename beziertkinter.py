26e31067b350a94b8af8ffb2aa2f5fc5189ca7ee
Frank Ling <ling.frank000@gmail.com>
2023-10-16 22:09:17 +0800

    increased rectangle sizes

--- beziertkinter.py
+++ beziertkinter.py
@@ -82,27 +82,37 @@ def draw_bezier():
         lines.append(l)
 
 
-def start_drag(event):
+def on_click(event):
+    rect = get_closest_rect(event.x, event.y)
     global drag_point
-    drag_point = event.widget
+    drag_point = rect
 
-def on_drag(event, rect):
-    x, y = get_center(canvas.coords(rect))
-    for l in lines:
-        canvas.delete(l)
-    canvas.move(rect, event.x - x, event.y - y)
-    store_bezier_in_array()
-    draw_bezier()
+
+def on_drag(event):
+    if drag_point is not None:
+        x, y = get_center(canvas.coords(drag_point))
+        for l in lines:
+            canvas.delete(l)
+        canvas.move(drag_point, event.x - x, event.y - y)
+        store_bezier_in_array()
+        draw_bezier()
+
+def get_closest_rect(x, y):
+    for r in start:
+        center = get_center(canvas.coords(r))
+        if abs(x - center[0]) < 8 and abs(y - center[1]) < 8:
+            return r
 
 def place_points(event):
     global start
     if len(start) <= 4:
-        rect = canvas.create_rectangle(event.x - 3, event.y - 3, event.x + 3, event.y + 3, fill="black")
+        rect = canvas.create_rectangle(event.x - 4, event.y - 4, event.x + 4, event.y + 4, fill="black")
         start.append(rect)
         if (len(start) == 4):
             canvas.unbind("<Button-1>")
             for p in start:
-                canvas.tag_bind(p, "<B1-Motion>", lambda event, r=rect: on_drag(event, r))
+                canvas.tag_bind(p, "<Button-1>", on_click)
+                canvas.tag_bind(p, "<B1-Motion>", on_drag)
             store_bezier_in_array()
             draw_bezier()
 
@@ -111,18 +121,3 @@ if __name__ == '__main__':
     canvas.bind("<Button-1>", place_points)
 
     root.mainloop()
-
-    # call bezier function and store them
-    # last_frame_time = 0
-    # while True:
-    #     current_time = time.time()
-    #     dt = current_time - last_frame_time
-    #     last_frame_time = current_time
-    #
-    #     sleepTime = 1. / 60 - (current_time - last_frame_time)
-    #     if sleepTime > 0:
-    #         time.sleep(sleepTime)
-    #
-    #     points = bezier(p1, p2, p3, p4, ts)
-    # win.getMouse()
-    # win.close()
