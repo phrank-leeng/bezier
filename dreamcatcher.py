79db6f56f4fd9ed446ae8d75a9f38fc45dfc8e29
phrank-leeng <ling.frank000@gmail.com>
2023-11-01 19:26:17 +0800

    dendrite spread working correctly

--- dreamcatcher.py
+++ dreamcatcher.py
@@ -26,6 +26,7 @@ right_f = Frame(root, width=250, height=700)
 right_f.pack(side='right')
 
 level = 1
+limited_lvl = level
 lvlStr = StringVar()
 
 # midpoint of circle frame
@@ -36,7 +37,7 @@ canvas = tk.Canvas(left_f, width=550, height=700, bg='gray55')
 canvas.place(x=0, y=0)
 canvas.pack()
 
-dendrites = []
+branches = []
 
 
 def dec_lvl():
@@ -54,10 +55,15 @@ def inc_lvl():
 
 
 def refresh():
+    global limited_lvl
+    limited_lvl = level if level < 7 else 6
     lvlStr.set(level)
-    for d in dendrites:
-        canvas.delete(d)
+    global branches
+    for b in branches:
+        canvas.delete(b)
+    branches = []
     RC_C()
+
 
 level_label = Label(right_f, textvariable=lvlStr)
 lvlStr.set(level)
@@ -78,43 +84,32 @@ def num_dendrites():
     return fibonacci(level + 2)
 
 
-def draw_dendrite(points):
-    color = "#%02x%02x%02x" % (135, 188, 240)
-
-
-def calc_points_on_circle():
-    points = []
-    # starting point 90-degree on circle
-    start = [250, 400]
-    points.append(start)
-    num_d = num_dendrites()
-    # angle between dendrites
-    angle = 360 / num_d
-    r = 150
-    for i in range(1, num_d):
-        theta = angle * i
-        points.append(point_unit_circle(start, r, theta))
-    return points
-
-
 def point_unit_circle(p, r, theta):
-    return [p[0] + r * math.sin(theta), p[1] + r * math.cos(theta)]
+    return [p[0] + r * math.cos(math.radians(theta)), p[1] + r * math.sin(math.radians(theta))]
 
 
 def RC_C():
-    points = calc_points_on_circle()
-    RC_A()
+    # draw the outer circle
+    canvas.create_oval(100, 50, 400, 350, outline='white', width=3)
+
+    # calculate the angle between dendrites
+    angle = 360 / num_dendrites()
+    # loop to make dendrites spread
+    for i in range(0, num_dendrites()):
+        RC_A(m_circle, 0 + angle * i, 0)
 
 
-def RC_A():
-    # draw branches, then transform points using circle calculation?
-    1 + 1
+def RC_A(start, theta, rec_depth):
+    if rec_depth < level and rec_depth < 5:
+        h1 = 150 / (1 + 0.75 * (limited_lvl - 1))
+        r = h1 * pow(0.75, rec_depth)
+        end = point_unit_circle(start, r, theta)
+        color = "#%02x%02x%02x" % (135 + rec_depth * 10, 188 + rec_depth * 5, 240)
+        branches.append(canvas.create_line(start[0], start[1], end[0], end[1], fill=color, width=10 - rec_depth * 2))
+        RC_A(end, theta, rec_depth + 1)
 
 
 # main method
 if __name__ == '__main__':
-    canvas.create_oval(100, 50, 400, 350, outline='white', width=3)
-
-    print(num_dendrites())
-
+    RC_C()
     root.mainloop()
