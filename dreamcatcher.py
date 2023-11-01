4f58c161fb14e98424664260726a9ea5287aab73
phrank-leeng <ling.frank000@gmail.com>
2023-11-01 20:49:49 +0800

    fixed direction of dendrites

--- dreamcatcher.py
+++ dreamcatcher.py
@@ -29,6 +29,7 @@ level = 1
 limited_lvl = level
 lvlStr = StringVar()
 
+circle = None
 # midpoint of circle frame
 m_circle = [250, 200]
 
@@ -63,6 +64,8 @@ def refresh():
         canvas.delete(b)
     branches = []
     RC_C()
+    if level > 5:
+        RC_B_1()
 
 
 level_label = Label(right_f, textvariable=lvlStr)
@@ -81,7 +84,8 @@ def fibonacci(n):
 
 
 def num_dendrites():
-    return fibonacci(level + 2)
+    # make dendrites stop spreading after level 6
+    return fibonacci(level + 2) if level < 7 else fibonacci(6 + 2)
 
 
 def point_unit_circle(p, r, theta):
@@ -89,24 +93,34 @@ def point_unit_circle(p, r, theta):
 
 
 def RC_C():
-    # draw the outer circle
-    canvas.create_oval(100, 50, 400, 350, outline='white', width=3)
+    if circle is None:
+        # draw the outer circle
+        canvas.create_oval(100, 50, 400, 350, outline='white', width=3)
 
     # calculate the angle between dendrites
     angle = 360 / num_dendrites()
     # loop to make dendrites spread
+    theta = 0
     for i in range(0, num_dendrites()):
-        RC_A(m_circle, 0 + angle * i, 0)
+        theta += angle
+        RC_A(point_unit_circle(m_circle, 150, theta), theta + 180, 0)
 
 
 def RC_A(start, theta, rec_depth):
-    if rec_depth < level and rec_depth < 5:
+    if rec_depth < limited_lvl:
         h1 = 150 / (1 + 0.75 * (limited_lvl - 1))
         r = h1 * pow(0.75, rec_depth)
         end = point_unit_circle(start, r, theta)
         color = "#%02x%02x%02x" % (135 + rec_depth * 10, 188 + rec_depth * 5, 240)
         branches.append(canvas.create_line(start[0], start[1], end[0], end[1], fill=color, width=10 - rec_depth * 2))
-        RC_A(end, theta, rec_depth + 1)
+        # left branch
+        RC_A(end, theta - 30, rec_depth + 1)
+        # right branch
+        RC_A(end, theta + 30, rec_depth + 1)
+
+
+def RC_B_1():
+    return
 
 
 # main method
